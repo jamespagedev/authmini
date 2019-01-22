8585fedfe285b5f502219db36887b7caedbca845
jamespagedev <jamespagedev@outlook.com>
2019-01-21 20:47:48 -0700

    fixed code duplication left by instructor Diandra

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ server.post('/api/register', (req, res) => {
 
   userCredentials.password = bcrypt.hashSync(userCredentials.password, 14); // overwrite password as soon as possible with the hash algorithm, 14 adds complexity to the hash algorithm (14 is a good baseline, go higher if needed... will require more log in time)
   db.addUser(userCredentials)
-    .insert(userCredentials)
     .then(ids => {
       res.status(201).json({ id: ids[0] });
     })
@@ -33,7 +32,6 @@ server.post('/api/login', (req, res) => {
   const creds = req.body;
 
   db.findByUsername(creds.username)
-    .where('username', creds.username)
     .first() // returns the first single object (containing the user found) in the array. If no objects were found, an empty array is returned.
     .then(user => {
       /* notes...
