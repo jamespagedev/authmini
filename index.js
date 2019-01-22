88dc8799cb8b692a7be369d86de55ef596ca2b13
jamespagedev <jamespagedev@outlook.com>
2019-01-21 18:34:38 -0700

    refactored helpers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
-const db = require('./database/dbConfig.js');
+const db = require('./database/helpers/dbHelpers.js');
 
 
 const server = express();
@@ -14,12 +14,13 @@ server.get('/', (req, res) => {
 });
 
 server.post('/api/register', (req, res) => {
-  const user = req.body;
-  user.password = bcrypt.hashSync(user.password, 14); // overwrite password as soon as possible with the hash algorithm, 14 adds complexity to the hash algorithm (14 is a good baseline, go higher if needed... will require more log in time)
-  db('users')
-    .insert(user)
-    .then(id => {
-      res.status(201).json({ id: id[0] });
+  const userCredentials = req.body;
+
+  userCredentials.password = bcrypt.hashSync(userCredentials.password, 14); // overwrite password as soon as possible with the hash algorithm, 14 adds complexity to the hash algorithm (14 is a good baseline, go higher if needed... will require more log in time)
+  db.addUser(userCredentials)
+    .insert(userCredentials)
+    .then(ids => {
+      res.status(201).json({ id: ids[0] });
     })
     .catch(err => {
       res.status(500).send(err);
@@ -29,22 +30,23 @@ server.post('/api/register', (req, res) => {
 server.post('/api/login', (req, res) => {
   // Precondition - Username must be unique
   // check that username exists AND that passwords match
-  const bodyUser = req.body;
+  const creds = req.body;
 
-  db('users')
-    .where('username', bodyUser.username)
-    .then(users => {
+  db.findByUsername(creds.username)
+    .where('username', creds.username)
+    .first() // returns the first single object (containing the user found) in the array. If no objects were found, an empty array is returned.
+    .then(user => {
       /* notes...
-        console.log('body user', bodyUser); // client password
-        console.log('body user', bcrypt.hashSync(bodyUser.password)); // notice different hash key gets created than what is stored in the db... to check hash is same for same password... use bcrypt.compareSync(clientPassword, dbHash)
+        console.log('body user', creds); // client password
+        console.log('body user', bcrypt.hashSync(creds.password)); // notice different hash key gets created than what is stored in the db... to check hash is same for same password... use bcrypt.compareSync(clientPassword, dbHash)
         console.log('database user', users[0]); // database hash password
       */
       // gets an array with a username and password...
       // and the hash matches between the db and the client
-      if (users.length && bcrypt.compareSync(bodyUser.password, users[0].password)) {
-        res.json({ info: "correct" })
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        res.json({ welcome: user.username })
       } else {
-        res.status(404).json({ err: "invalid username or password" })
+        res.status(401).json({ err: "invalid username or password" })
       }
     })
     .catch(err => res.status(500).send(err));
